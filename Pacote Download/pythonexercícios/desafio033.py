f07518b0a238f43468837ad777e2ccba0316d9ff
xXEKLEKTOSXx <braianek777@gmail.com>
2025-06-06 17:00:57 -0300

    Conclusão do desafio033

--- Pacote Download/pythonexercícios/desafio033.py
+++ Pacote Download/pythonexercícios/desafio033.py
@@ -3,8 +3,15 @@ b = int(input('Segundo valor: '))
 c = int(input('Terceiro valor: '))
 # Verificando que é menor
 menor = a
-if b<a and b<c:
+if b < a and b < c:
     menor = b
-if c<a and c<b:
+if c < a and c < b:
     menor = c
+# Verificando quem é o maior
+maior = a
+if b > a and b > c:
+    maior = b
+if c > a and c > b:
+    maior = c
 print('O menor valor digitado foi {}'.format(menor))
+print('O maior valor digitado foi {}'.format(maior))
