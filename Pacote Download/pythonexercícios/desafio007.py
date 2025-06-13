6ea0fab82b45b08444b3273d370c93b983ec5320
xXEKLEKTOSXx <braianek777@gmail.com>
2025-06-13 14:49:47 -0300

    coloridos

--- Pacote Download/pythonexercícios/desafio007.py
+++ Pacote Download/pythonexercícios/desafio007.py
@@ -1,4 +1,13 @@
 n1 = float(input('Primeira nota: '))
 n2 = float(input('Segunda nota: '))
 media = (n1 + n2) / 2
-print(' A primeira nota foi {}, a segunda nota foi {}, a média então é {:.1f}.'.format(n1, n2, media))
+cores = {'limpa':'\033[m', 
+         'branco':'\033[0;30m', 
+         'vermelho':'\033[0;31m', 
+         'verde':'\033[0;32m', 
+         'amarelo':'\033[0;33m', 
+         'azul':'\033[0;34m', 
+         'roxo':'\033[0;35m', 
+         'azulclaro':'\033[0;36m', 
+         'cinza':'\033[0;37m'}
+print(' A primeira nota foi {}{}{}, a segunda nota foi {}{}{}, a média então é {}{:.1f}{}.'.format(cores['amarelo'], n1, cores['limpa'], cores['azul'], n2, cores['limpa'], cores['verde'], media, cores['limpa']))
--- Pacote Download/pythonexercícios/desafio008.py
+++ Pacote Download/pythonexercícios/desafio008.py
@@ -6,5 +6,13 @@ m = n * 1
 dm = n * 10
 cm = n * 100
 mm = n * 1000
-
-print('A medida de {}m corresponde a:\n{:.3f}km\n{:.2f}hm\n{:.1f}dam\n{:.0f}dm\n{:.0f}cm\n{:.0f}mm'.format(n, km, hm, dam, dm, cm, mm))
+cores = {'limpa':'\033[m', 
+         'branco':'\033[0;30m', 
+         'vermelho':'\033[0;31m', 
+         'verde':'\033[0;32m', 
+         'amarelo':'\033[0;33m', 
+         'azul':'\033[0;34m', 
+         'roxo':'\033[0;35m', 
+         'azulclaro':'\033[0;36m', 
+         'cinza':'\033[0;37m'}
+print('A medida de {}m corresponde a:\n{}{:.3f}{}km\n{}{:.2f}{}hm\n{}{:.1f}{}dam\n{}{:.0f}{}dm\n{}{:.0f}{}cm\n{}{:.0f}{}mm'.format(n, cores['vermelho'], km, cores['limpa'], cores['verde'], hm, cores['limpa'], cores['amarelo'], dam, cores['limpa'], cores['azul'], dm, cores['limpa'], cores['roxo'], cm, cores['limpa'], cores['azulclaro'], mm, cores['limpa']))
