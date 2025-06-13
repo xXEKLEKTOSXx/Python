87ac7e899e2ad00eea35513e2961fae060710835
xXEKLEKTOSXx <braianek777@gmail.com>
2025-06-13 14:08:12 -0300

    desafio006 colorido

--- Pacote Download/pythonexercícios/desafio006.py
+++ Pacote Download/pythonexercícios/desafio006.py
@@ -5,4 +5,4 @@ t = 3
 s1 = n * d
 s2 = n * t
 s3 = n ** (1/2)
-print('O número é {}, vezes 2 é {}, Vezes 3 é {}, e sua raiz quadrada é {:.2f}'.format(n, s1, s2, s3))
+print('O número é {}{}{}, vezes 2 é {}{}{}, Vezes 3 é {}{}{}, e sua raiz quadrada é {}{:.2f}{}'.format('\033[0;34m', n, '\033[m', '\033[0;33m',  s1, '\033[m', '\033[0;31m', s2, '\033[m', '\033[0;30;40m', s3, '\033[m'))
