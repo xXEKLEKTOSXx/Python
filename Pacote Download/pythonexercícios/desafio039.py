fa5038e6c763c7d8af88f4bfca2d44bf083f960f
xXEKLEKTOSXx <braianek777@gmail.com>
2025-06-27 17:50:00 -0300

    novo2

--- Pacote Download/pythonexercícios/desafio039.py
+++ Pacote Download/pythonexercícios/desafio039.py
@@ -1,10 +1,18 @@
 from datetime import date
-nascimento = int(input('Digite o ano de nascimento: '))
-if nascimento == 0:
-    nascimento = date.today().year
-if nascimento < 18:
-    print('Ainda vai se alistar ao serviço militar')
-elif nascimento == 18:
-    print('Hora de se alistar')
-else:
-    print('Passou do tempo do alistamento')
+atual = date.today().year
+nasc = int(input('Ano de nascimento: '))
+idade = atual - nasc
+print('Quem nasceu em {} tem {} anos em {}'.format(nasc, idade, atual))
+if idade == 18:
+    print('Você tem que se alistar IMEDIATAMENTE!')
+elif idade < 18:
+    saldo = 18 - idade
+    print('Ainda faltam {} anos para o alistamento'.format(saldo))
+    ano = atual + saldo
+    print('Seu alistamento será em {} ano'.format(ano))
+elif idade > 18: 
+    saldo = idade - 18
+    print('Você já deveria ter se alistado há {} anos'.format(saldo))
+    ano = atual - saldo 
+    print('Seu alistamento foi em {}'.format(ano))
+    
