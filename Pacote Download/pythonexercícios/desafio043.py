68623d6e1813d84754ab1a15339ee5d7b72fe567
xXEKLEKTOSXx <braianek777@gmail.com>
2025-07-02 18:26:47 -0300

    novo3

--- Pacote Download/pythonexercícios/desafio043.py
+++ Pacote Download/pythonexercícios/desafio043.py
@@ -1,4 +1,14 @@
-peso = float(input('Digite o peso: '))
-altura = float(input('Digite a altura: '))
-imc = (altura ** (1/2)) / peso
-print('O IMC é {}'.format(imc))
+peso = float(input('Qual é seu peso: (Kg) '))
+altura = float(input('Qual é sua altura: (m) '))
+imc = peso / (altura ** 2)
+print('O IMC dessa pessoa é {:.1f}'.format(imc))
+if imc < 18.5:
+    print('Você está ABAIXO DO PESO normal')
+elif 18.5 <= imc < 25:
+    print('PARABÉNS, você esta na faixa de PESO NORMAL')
+elif 25 <= imc < 30:
+    print('Você está em SOBREPESO')
+elif 30 <= imc < 40:
+    print('Você está em OBESIDADE')
+elif imc >= 40:
+    print('Você está em OBESIDADE MÓRBIDA, cuidado!')
