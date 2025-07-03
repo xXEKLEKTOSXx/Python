f1510648945cf4bf6fb2be58e52ea92da522ebc8
xXEKLEKTOSXx <braianek777@gmail.com>
2025-07-03 18:02:26 -0300

    novo4

---
+++ Pacote Download/pythonexercícios/desafio044.py
@@ -0,0 +1,29 @@
+print('{:=^40}'.format(' LOJA GUANABARA '))
+preço = float(input('Preço das compras: R$'))
+print('''FORMAS DE PAGAMENTO
+[ 1 ] à vista dinheiro/cheque
+[ 2 ] à vista cartão
+[ 3 ] 2x no cartão
+[ 4 ] 3x ou mais no cartão''')
+opção = int(input('Qual é a opção? '))
+if opção == 1:
+    total = preço - (preço * 10/100)
+elif opção == 2:
+    total = preço - (preço * 5 / 100)
+elif opção == 3:
+    total = preço
+    parcela = total / 2
+    print('Sua compra será parcelada em 2x de R${} SEM JUROS'.format(parcela))
+elif opção == 4:
+    total = preço + (preço * 20 / 100)
+    totparc = int(input('Quantas parcelas? '))
+    parcela = total / totparc
+    print('Sua compra será parcelada em {}x de R${:.2f}'.format(totparc, parcela))
+    print('Sua compra de R${:.2f} vai custar R${:.2f} COM JUROS.'.format(preço, total))
+else:
+    total = preço
+    print('OPCÃO INVALIDA de pagamento. Tente novamente!')
+print('Sua compra de R${:.2f} vai custar R${} no final.'.format(preço, total))
+
+
+
